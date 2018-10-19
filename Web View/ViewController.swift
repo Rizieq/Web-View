0c7192f7555ec1a9de936f5292e70db8c6d5ab8b
Macbook Pro <macbookpro@rizieq-macbook.local>
2018-10-19 08:45:08 +0700

    Initial Commit

---
+++ Web View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Web View
+//
+//  Created by Macbook Pro on 19/10/18.
+//  Copyright © 2018 Macbook Pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
